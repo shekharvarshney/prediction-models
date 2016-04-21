e009df29ce8a4097743314aab6e2d89d4937d315
shekhar varshney <shekhar.varshney@gmail.com>
2016-04-21 22:42:06 +0200

    Skeletal Spring Boot project

---
+++ src/main/java/com/precioustech/fxtrading/prediction/PredictionModelsApplication.java
@@ -0,0 +1,12 @@
+package com.precioustech.fxtrading.prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionModelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionModelsApplication.class, args);
+	}
+}
